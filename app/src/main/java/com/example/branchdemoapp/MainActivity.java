299c38cf53b60caa3001ced7134c004f2ce26f07
vamsimadhav <vamsimadhav2000@gmail.com>
2023-05-28 23:10:05 +0530

    Added Splash Screen

--- app/src/main/java/com/example/branchdemoapp/MainActivity.java
+++ app/src/main/java/com/example/branchdemoapp/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.branchdemoapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
+import io.branch.referral.Branch;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Branch.getAutoInstance(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Branch branch = Branch.getInstance();
+
+        // Branch init
+        branch.initSession((referringParams, error) -> {
+            if (error == null) {
+                Log.i("BRANCH SDK", referringParams.toString());
+            } else {
+                Log.i("BRANCH SDK", error.getMessage());
+            }
+        }, this.getIntent().getData(), this);
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        this.setIntent(intent);
     }
 }
